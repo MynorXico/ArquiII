7fc5c07c8f92e3dad6538fc56ec997c5f114a7c0
Mynor Xico <mynorottoniel.xico2@gmail.com>
2018-11-20 06:05:39 -0600

    Sensor de temperatura

--- TemperatureSensor.py
+++ TemperatureSensor.py
@@ -5,21 +5,22 @@ import time
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+
+device_file = glob.glob('/sys/bus/w1/devices/' + '28*')[0] + '/w1_slave'
  
-def read_temp_raw():
+
+
+def readfile():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
  
-def read_temp():
-    lines = read_temp_raw()
+def getTemperatura():
+    lines = readfile()
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
-        lines = read_temp_raw()
+        lines = readfile()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
@@ -28,6 +29,6 @@ def read_temp():
         return temp_c, temp_f
 	
 while True:
-	print(read_temp())	
+	print(getTemperatura())	
 	time.sleep(1)
 	
