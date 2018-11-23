b0dc8a0ec93c442a8f1c10f357ab2c788d2e3ca9
Unknown <hscaballeros@correo.url.edu.gt>
2018-11-22 19:32:50 -0600

    Motor

--- Motores.py
+++ Motores.py
@@ -13,11 +13,11 @@ GPIO.setup(35,GPIO.OUT)
 GPIO.setup(37,GPIO.OUT)
 
 #LEFT
-#MOTOR 3 LF
+#MOTOR 3 IF
 GPIO.setup(32,GPIO.OUT)
 GPIO.setup(36,GPIO.OUT)
 
-#MOTOR 4 LR
+#MOTOR 4 IT
 GPIO.setup(38,GPIO.OUT)
 GPIO.setup(40,GPIO.OUT)
 
@@ -62,10 +62,10 @@ def left(seconds):
     
 def backwards(seconds):
     print('Backwards')
-    GPIO.output(31,GPIO.LOW)
-    GPIO.output(33,GPIO.HIGH)
-    GPIO.output(35,GPIO.HIGH)
-    GPIO.output(37,GPIO.LOW)
+    GPIO.output(31,GPIO.HIGH)
+    GPIO.output(33,GPIO.LOW)
+    GPIO.output(35,GPIO.LOW)
+    GPIO.output(37,GPIO.HIGH)
     GPIO.output(32,GPIO.HIGH)
     GPIO.output(36,GPIO.LOW)
     GPIO.output(38,GPIO.LOW)
@@ -74,10 +74,10 @@ def backwards(seconds):
     
 def forward(seconds):
     print('Forward')
-    GPIO.output(31,GPIO.HIGH)
-    GPIO.output(33,GPIO.LOW)
-    GPIO.output(35,GPIO.LOW)
-    GPIO.output(37,GPIO.HIGH)
+    GPIO.output(31,GPIO.LOW)
+    GPIO.output(33,GPIO.HIGH)
+    GPIO.output(35,GPIO.HIGH)
+    GPIO.output(37,GPIO.LOW)
     GPIO.output(32,GPIO.LOW)
     GPIO.output(36,GPIO.HIGH)
     GPIO.output(38,GPIO.HIGH)
@@ -106,8 +106,21 @@ def move(newX,newY):
     y = newY
 
 clean()
-GPIO.output(32,GPIO.LOW)
-GPIO.output(36,GPIO.HIGH)
-time.sleep(10)
+forward(2.76)
+stop(0.5)
+right(1.05)
+stop(0.5)
+forward(2.76)
+stop(0.5)
+backwards(2.76)
+stop(0.5)
+right(1.05)
+stop(0.5)
+forward(2.76)
+stop(0.5)
+right(2.1)
+#stop(0.5)
+#left(1.3)
+clean()
 print("Ended")
 GPIO.cleanup()
