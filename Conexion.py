302cbafe6a9a824b07492c12a8518dd8ba14180e
HSCO2017 <hscaballeros@correo.url.edu.gt>
2018-11-25 22:27:33 -0600

    Inserta sensores correctamente

--- Conexion.py
+++ Conexion.py
@@ -5,11 +5,11 @@ client = MongoClient('mongodb://admin:1admin@ds263500.mlab.com:63500/arquitwo')
 db = client['arquitwo']
 
 #Metodo que todos lo grupos deben usar para insertar a la base de datos
-def InsertarXplorer(dev_id,sen_id, posX, posY, value):
+def InsertarXplorer(sen_id, posX, posY, value):
     collection = db['XPlorer']
     collection.insert_one({
         "Date": datetime.datetime.now(),
-        "Device_ID": dev_id,
+        "Device_ID": 1,
         "Sensor_ID": sen_id,
         "X": posX,
         "Y": posY,
--- MainSensores.py
+++ MainSensores.py
@@ -1,7 +1,7 @@
 import SensorLuz
 import TemperaturaHumedad
 import TemperatureSensor
-import SensorProx
+#import SensorProx
 import Conexion
 import time
 
@@ -11,10 +11,12 @@ GPIOHumedad = 23
 
 dev_id = 1					# Id del grupo
 
-sen_id_luz = -1	
-sen_id_humedad = -1
-sen_id_temperatura = -1
-sen_id_distancia = -1
+sen_id_luz = 3	
+sen_id_humedad = 2
+sen_id_temperatura = 1
+#sen_id_distancia = 4
+
+sensores = ["", "Temperatura", "Humedad", "Luz"]
 
 xactual = -1
 yactual = -1
@@ -30,15 +32,18 @@ def ObtenerXY():
 def InsertarDato(sen_id, value):
     global xactual
     global yactual
-    InsertarXplorer(sen_id, xactual, yactual, value)
-    print("Se insertó valor de sensores ")
+    Conexion.InsertarXplorer(sen_id, xactual, yactual, value)
+    print("Se insertó valor de " + str(value) + " para " + sensores[sen_id])
 
-	
-	
 def Insertar2Minutos():
-	while True:
-		InsertarDato(sen_id_luz, SensorLuz.GetLight(GPIOLuz))
-		InsertarDato(sen_id_humedad, TemperaturaHumedad.GetHumedad(GPIOHumedad))
-		InsertarDato(sen_id_temperatura, TemperatureSensor.getTemperatura())
-		InsertarDato(sen_id_distancia, SensorProx.getDistance())
-		time.sleep(2)
+	startingTime = time.time()
+	while(time.time()<=startingTime+120):
+            inicia = time.time()
+            InsertarDato(sen_id_luz, SensorLuz.GetLight(GPIOLuz))
+            InsertarDato(sen_id_humedad, TemperaturaHumedad.GetHumedad(GPIOHumedad))
+            InsertarDato(sen_id_temperatura, TemperatureSensor.getTemperatura())
+            #InsertarDato(sen_id_distancia, SensorProx.getDistance())
+            while(time.time() <= inicia+2): 
+                print("waiting")
+ObtenerXY()
+Insertar2Minutos()
